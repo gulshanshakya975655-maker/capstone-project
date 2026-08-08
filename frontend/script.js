57406cd7e166df636ecf0ba162a6f7b7fcd47f91
Gulshan <test@example.com>
2026-08-08 08:54:16 +0530

    Update frontend to point to deployed backend URL

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://capstone-project-psw7.onrender.com";
 
 const taskForm = document.getElementById("task-form");
 const taskListEl = document.getElementById("task-list");
